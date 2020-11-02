a80bf5f6d2b810befae51c3f5864aae5357cf649
Josef Glatz <j.glatz@supseven.at>
2020-11-02 15:36:03 +0100

    [TASK] Add infos about Little Snitch actual not availability within homebrew

--- fresh.sh
+++ fresh.sh
@@ -13,3 +13,9 @@ brew update
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
+
+# Inform about not installable applications
+echo ""
+echo "obdev Little Snitch for Catalina needs to be installed manually https://obdev.at/downloads/littlesnitch/LittleSnitch-4.6.dmg"
+echo "obdev Little Snitch for Big Sur needs to be installed manually https://obdev.at/downloads/littlesnitch/LittleSnitch-5.0.dmg"
+echo ""
