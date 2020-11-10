546cb38133c6d64bc6802bd571b74a17b4a665ee
Josef Glatz <josefglatz@gmail.com>
2020-11-10 23:53:06 +0100

    Aliases: add massively amount of new shortcuts

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -6,9 +6,25 @@ alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-
+alias xattr-delete="sudo xattr -r -d com.apple.quarantine"
+alias cpwd="copydir"
+alias cask="brew cask"
+alias zip="zip -x *.DS_Store -x *__MACOSX* -x *.AppleDouble*"
+# Recursively remove Apple meta files
+alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
+alias cleanupad="find . -type d -name '.AppleD*' -ls -exec /bin/rm -r {} \;"
 # Lock the screen (when going AFK)
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+# Quick-Look preview files from the command line
+alias ql="qlmanage -p &>/dev/null"
+# Kill all the tabs in Chrome to free up memory
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+# Show system information
+alias displays="system_profiler SPDisplaysDataType"
+alias cpu="sysctl -n machdep.cpu.brand_string"
+alias ram="top -l 1 -s 0 | grep PhysMem"
+
+alias pbspaces="pbpaste | expand | pbcopy"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
