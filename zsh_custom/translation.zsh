a3cbcb4d61523913b9596f0d9e139b3f60c78a0c
Josef Glatz <josefglatz@gmail.com>
2020-11-12 20:24:57 +0100

    Aliases: translate de<->en (free version)

---
+++ zsh_custom/translation.zsh
@@ -0,0 +1,7 @@
+# translation english to german `transe`
+alias transe="trans -e google en:de"
+# translate german to english `transd`
+alias transd="trans -e google de:en"
+alias tre="transe"
+alias trd="transd"
+alias tr="transd"
