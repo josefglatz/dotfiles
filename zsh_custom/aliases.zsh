1b4067f5ca42db05f3f80458fdb944a6cd86e145
Josef Glatz <josefglatz@gmail.com>
2020-11-16 08:58:28 +0100

    Aliases: move git related into own file

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -56,22 +56,3 @@ alias vrebuild="vagrant destroy --force && vagrant up"
 
 # Docker
 alias docker-composer="docker-compose"
-
-# Git
-alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
-#alias gl="git log --oneline --decorate --color"
-alias amend="git add . && git commit --amend --no-edit"
-alias commit="git add . && git commit -m"
-alias diff="git diff"
-alias force="git push --force"
-alias nuke="git clean -df && git reset --hard"
-alias pop="git stash pop"
-alias pull="git pull"
-alias push="git push"
-alias resolve="git add . && git commit --no-edit"
-alias stash="git stash -u"
-alias unstage="git restore --staged ."
-alias cdgr='cd "$(git root)" && echo "Changed directory to actual git root $(pwd)"'
-#alias wip="commit wip"
---
+++ zsh_custom/aliases_git.zsh
@@ -0,0 +1,18 @@
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+#alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias cdgr='cd "$(git root)" && echo "Changed directory to actual git root $(pwd)"'
+#alias wip="commit wip"
