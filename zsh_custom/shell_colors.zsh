058946874a60fe163e9c3f1898e44f7cbce720ec
Josef Glatz <josefglatz@gmail.com>
2020-11-16 06:33:56 +0100

    Shell Environmant Variables for colors
    e.g.
    
    `echo "${RED}das ist rot ${RESET}Das ist normal ${BOLD_YELLOW} das ist gelb bold${RESET}"`

---
+++ zsh_custom/shell_colors.zsh
@@ -0,0 +1,13 @@
+# Script initially found at https://pthree.org/2009/12/18/add-colors-to-your-zsh-scripts/
+if [[ "$terminfo[colors]" -gt 8 ]]; then
+    colors
+fi
+
+for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+    eval $COLOR='$fg_no_bold[${(L)COLOR}]'
+    eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
+done
+eval RESET='$reset_color'
+
+# Now use it like following example: 
+# echo "${RED}das ist rot ${RESET}Das ist normal ${BOLD_YELLOW} das ist gelb bold${RESET}"
