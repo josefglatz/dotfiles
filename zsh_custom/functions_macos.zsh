c85927917afadef68af9a2f4eda8209c47e609b8
Josef Glatz <josefglatz@gmail.com>
2020-11-24 12:34:53 +0100

    macos functions for plists

--- zsh_custom/functions_macos.zsh
+++ zsh_custom/functions_macos.zsh
@@ -5,4 +5,21 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     function log-stream-info-process {
         log stream --info --predicate 'process=="$1"'
     }
+
+    # Find problematic plist files with plutil
+    # ----------------------------------------
+    function plist-check {
+        echo "Searching for problematic plist files:"
+        plutil -s $HOME/Library/Preferences/*.plist
+
+        echo "\n\n  You can run \"plist-delete-problematic\" to delete found problematic plist files."
+    }
+    alias defaults-check="plist-check"
+
+    # Delete problematic plist files
+    # ------------------------------
+    function plist-delete-problematic {
+        trash `plutil -s $HOME/Library/Preferences/*.plist | grep 'plist:' | sed 's/://g'`
+    }
+    alias defaults-delete-problematic="plist-delete-problematic"
 fi
