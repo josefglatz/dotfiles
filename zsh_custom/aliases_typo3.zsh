58f684a0faeb367db2d8c97dd0b877639257eb9c
Josef Glatz <josefglatz@gmail.com>
2020-12-22 21:16:27 +0100

    Aliases: streamline TYPO3 aliases

--- zsh_custom/aliases_typo3.zsh
+++ zsh_custom/aliases_typo3.zsh
@@ -3,8 +3,7 @@
 #
 
 # composer update typo3/cms core packages in an existing project
-alias ct3u="composer u typo3/cms-* --with-dependencies"
-
+alias typo3-composer-update-core-packages="composer u typo3/cms-* --with-all-dependencies"
 
 
 #
@@ -14,9 +13,9 @@ alias ct3u="composer u typo3/cms-* --with-dependencies"
 # grep multiple domains recursively in PWD and output string between single quotes
     # Built for getting all absolute domains in TYPO3 site configuration files when executed
     # in "$(git root)/app/config/sites" folder:
-alias typo3sitesdomains="grep -R 'http' . | sed -n \"s/^.*'\(.*\)'.*$/\1/p\" | sort"
+alias typo3-sites-domains="grep -R 'http' . | sed -n \"s/^.*'\(.*\)'.*$/\1/p\" | sort"
 
 # grep used locale values, remove duplicate entries and sort alphabetically
     # Built for getting all locale values within TYPO3 site configuration files when executed
     # in "$(git root)/app/config/sites" folder:
-alias typo3siteslocales="grep -R 'locale' . | sed -n \"s/^.*locale\:\(\)/\1/p\" | uniq -u | sort"
+alias typo3-sites-locales="grep -R 'locale' . | sed -n \"s/^.*locale\:\(\)/\1/p\" | uniq -u | sort"
