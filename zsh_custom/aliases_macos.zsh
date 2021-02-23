d34ae98a1acea8136a2f56994ba8af9d3d7af938
Josef Glatz <josefglatz@gmail.com>
2021-02-23 11:37:29 +0100

    [FEATURE] Aliases macOS: related ~/Downloads aliases
    - dl= change dir into my downloads folder
    
    - rdn= list downloads files sorted by newer first
    
    - rdn10= list latest 10 files in downloads folder
    
    - rdnt= list all new files in download folder from today

--- zsh_custom/aliases_macos.zsh
+++ zsh_custom/aliases_macos.zsh
@@ -1,3 +1,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	alias log-stream-info="log-stream-info-process"
+
+	# CD into my Downloads folder
+	alias dl="cd $HOME/Downloads"
+	# List my Downloads folder with newest first
+	alias rdn="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color -t $HOME/Downloads"
+		# Limit to last 10 files
+		alias rdn10="rdn | head -10"
+	alias rdnt="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --time-style=+%Y-%m-%d --color -t $HOME/Downloads  | grep $(date +%Y-%m-%d)"
 fi
